1a21f08b442848cd6a841da63edea46245e74bb6
Danae <danae678@gmail.com>
2022-01-26 15:30:17 -0500

    fixed login page used react native lol my bad

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react-native'
+import { Amplify } from 'aws-amplify';
 
-Amplify.configure(awsconfig);
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-export default withAuthenticator(App)
+export default withAuthenticator(App);
