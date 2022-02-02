32936c6911b4b16f2b8003ba1e9fe6578117f12b
Solomon Lucas <soluke22@gmail.com>
2022-02-02 16:14:46 -0500

    it runs on my branch yay

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ Amplify.configure(awsExports);
 
 export default function App() {
   return (
-    <Authenticator socialProviders={['apple', 'facebook', 'google']}>
+    <Authenticator socialProviders={['apple','google']}>
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user.username}</h1>
