0724005f47badf4ae775f8d43c6cae47dc5c633b
Danae <danae678@gmail.com>
2022-02-28 17:10:46 -0500

    fixes

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,18 @@
 import { Amplify } from 'aws-amplify';
 
-import { Authenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-export default function App() {
+function App({ signOut, user }) {
   return (
-    <Authenticator socialProviders={['apple', 'facebook', 'google']}>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
+
+export default withAuthenticator(App);
