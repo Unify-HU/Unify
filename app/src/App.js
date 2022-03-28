8b05095bb4027d3b926f17c38a3ff9e9c7e07522
Solomon Lucas <soluke22@gmail.com>
2022-03-28 14:58:20 -0400

    home page fix

--- app/src/App.js
+++ app/src/App.js
@@ -7,10 +7,14 @@ import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App() {
+function App({ signOut, user }) {
   return (
+    <>
+    <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
       <Home />
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
