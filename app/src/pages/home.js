aeba6ad2209299b69a917739786c7d6701ccd6d6
Solomon Lucas <soluke22@gmail.com>
2022-03-30 15:42:00 -0400

    changes

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -7,8 +7,10 @@ export default function Home() {
     return (
       
       <div>
-        <NavBar/>
+        <NavBar />
+        <center>
         <BusinessCollection />
+        </center>
       </div>
     ) 
   }
